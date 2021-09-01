12a2cbe2e1d116a9e6b0d8befdd6e63efdf32397
darrenwshort <w1765868@apps.losrios.edu>
2021-08-31 19:45:47 -0700

    .

--- day_1/practice.py
+++ day_1/practice.py
@@ -8,10 +8,26 @@ person_1 = {
     "wears_glasses": True
     }
 
+print(person_1) #print to see what are current contents
+
+
 # verify the type of person_1 to be a dictionary by using type
+print(type(person_1))
 
 # add a key value pair to person_1 with the last_name of Doo
+person_1["last_name"] = "Doo"
+
+print(person_1) #print dict to see addition of last name
 
 # update person_1 favorite_snack to "Scooby Snacks"
+person_1.update({"favorite_snack": "Scooby Snacks"})
+
+print(person_1) #print dict to see update to favorite snack
 
 # Remove the "wears_glasses" key:value from person_1
+del person_1["wears_glasses"]
+
+print(person_1) #print to see deletion of wears_glasses
+
+
+
--- day_2/exercise.py
+++ day_2/exercise.py
@@ -7,8 +7,54 @@
 #     - A function that multiplies three integers
 #     - A function that adds four integers
 
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2, num3):
+    return num1 * num2 * num3
+
+def addition(num1, num2, num3, num4):
+    return num1 + num2 + num3 + num4
+
+
 # Medium: 
 #     - Create a calculator function using THREE input parameters (two float, one string) to all the user to add, substract, multiply and divide.
+float1 = float(input("Enter 1st float: "))
+float2 = float(input("Enter 2nd float: "))
+str1 = input("Enter a string: ")
+
+def calculator(f1, f2, s1):
+    print(f1, f2, s1)
+
+calculator(float1, float2, str1)
 
 # Hard: 
 #     - You're at a restaurant with some friends and the server didn't split up the check.  Create a function that takes a bill amount, multiplies it by a global variable called tax_rate, adds the tax, and then divides the total bill by the number of people input by the user.  BONUS:  Include an option for adding tip through either a percentage amount assigned to a global varible, or through a specific amount input by the user.
+
+tax_rate = 0.0
+tip_perc = 0.0
+
+bill_amt = float(input("Enter bill amount: "))
+tax_rate = float(input("Enter tax rate (in percent): "))
+tip_perc = float(input("Enter tip (in percentage; 0 if you're a cheapskate): "))
+diners = float(input("How may people are dining tonight? "))
+
+
+def calc_bill(ba):
+    tax_amount = ba * (tax_rate / 100)
+    tip_amount = ba * (tip_perc / 100)
+    total_bill = ba + tax_amount + tip_amount
+    return total_bill, tax_amount, tip_amount
+
+
+tb, ta, tpa = calc_bill(bill_amt)
+print("\nBill Breakdown:")
+print("---------------")
+print("Bill Amount (before tax & tip): $", bill_amt)
+print("Tax: $", ta, "(", tax_rate, "%)")
+print("Tip: $", tpa, "(", tip_perc, "%)")
+print("Total Cost: $", tb)
+print("Cost per person: $", tb / diners)
+print()
+
